9accfb3b35d27998f70ef045653e2e34a4e01a97
Iridium <luk.rau.01@gmail.com>
2018-12-04 18:26:16 +0100

    JavaFX initial commit

---
+++ JavaFX_Practice_01/src/module-info.java
@@ -0,0 +1,6 @@
+module JavaFX_Practice_01 {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ JavaFX_Practice_01/src/sample/Main.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX Practice.");
+//        root.getChildren().add(greeting);
+//        greeting.setTextFill(Color.NAVY);
+//        greeting.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+
+        primaryStage.setTitle("JavaFX Practice");
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
