a3b756d60443356bce1b1197e1131e0b34db7b7d
Iridium <luk.rau.01@gmail.com>
2018-12-22 01:51:21 +0100

    TodoItem datamodel added. Basic layout created.

--- JavaFX_Practice_02/src/sample/Controller.java
+++ JavaFX_Practice_02/src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import sample.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Default short description", "Default details", LocalDate.now());
+        TodoItem item2 = new TodoItem("Second short description", "Full details of second item", LocalDate.of(2019, Month.JANUARY, 1));
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        System.out.println("Selected item is " + item);
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n");
+        sb.append("Due: ");
+        sb.append(item.getDeadline().toString());
+
+        itemDetailsTextArea.setText(sb.toString());
+    }
 }
