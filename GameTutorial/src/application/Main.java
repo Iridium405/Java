2c62a43eae357979d1f439f0cd3f2b58d1e46bb9
Iridium <luk.rau.01@gmail.com>
2019-01-22 13:04:06 +0100

    initial commit

---
+++ GameTutorial/src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.ViewManager;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+            ViewManager manager = new ViewManager();
+            primaryStage = manager.getMainStage();
+            primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ GameTutorial/src/view/ViewManager.java
@@ -0,0 +1,39 @@
+package view;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ViewManager {
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    private AnchorPane mainPane;
+    private Scene mainScene;
+    private Stage mainStage;
+
+
+    public ViewManager() {
+        this.mainPane = new AnchorPane();
+        this.mainScene = new Scene(mainPane, WIDTH, HEIGHT);
+        this.mainStage = new Stage();
+        mainStage.setScene(mainScene);
+//        createButtons();
+    }
+
+
+    public Stage getMainStage() {
+        return mainStage;
+    }
+
+
+    private void createButtons() {
+        Button button1 = new Button();
+        button1.setDefaultButton(true);
+        mainPane.getChildren().add(button1);
+    }
+}
